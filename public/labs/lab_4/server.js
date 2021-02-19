e1e3aa2ff095f88fddfdfd0c235b9df3fc3bef0d
Edward Aloba <ealoba@terpmail.umd.edu>
2021-02-19 14:47:01 -0500

    added server

---
+++ public/labs/lab_4/server.js
@@ -0,0 +1,19 @@
+function onLoad(){
+    console.log('script loaded');
+}
+
+window.onload = onLoad;
+
+app.route('/api')
+.get(async(req,res) => {
+    console.log('GET request detected');
+    const data = await('https://data.princegeorgescountymd.gov/resource/vkdv-rvfx.json');
+    const json = await data.json();
+    console.log('data from fetch',json);
+    res.json(json);
+})
+    .post(async(req,res) => {
+        console.log('POST request detected');
+        console.log('Form data in res.body',req.body);
+        res.send({message: 'hello world'});
+    })
